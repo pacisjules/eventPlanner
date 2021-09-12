9a85554e6eae7d681743e973dd04b2ca478a622a
Ishimwe Jules Pacis <pacisjules@gmail.com>
2021-09-12 13:30:17 +0200

    Initial commit

---
+++ src/main/java/com/event_Planner/eventPlanner/EventPlannerSystemApplication.java
@@ -0,0 +1,13 @@
+package com.event_Planner.eventPlanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventPlannerSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventPlannerSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/event_Planner/eventPlanner/EventPlannerSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.event_Planner.eventPlanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventPlannerSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
